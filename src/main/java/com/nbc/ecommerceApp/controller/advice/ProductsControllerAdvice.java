32040e1ec27ee24a130e06a3dbdc1f63443a40b8
Tejesh <nagatejesh@users.noreply.github.com>
2024-07-14 09:54:34 +0000

    Data validation for Product

--- src/main/java/com/nbc/ecommerceApp/controller/advice/ProductsControllerAdvice.java
+++ src/main/java/com/nbc/ecommerceApp/controller/advice/ProductsControllerAdvice.java
@@ -4,9 +4,14 @@ import com.nbc.ecommerceApp.exception.ErrorResponse;
 import com.nbc.ecommerceApp.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ProductsControllerAdvice {
@@ -16,6 +21,21 @@ public class ProductsControllerAdvice {
     public ErrorResponse handleProductNotFoundException(ProductNotFoundException ex){
         ErrorResponse response = new ErrorResponse(ex);
         return response;
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String, String> res = new LinkedHashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(err->{
+            res.put(err.getField(), err.getDefaultMessage());
+        });
+        return res;
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException ex){
+        ErrorResponse res = new ErrorResponse("Invalid argument entered", ex);
+        return res;
     }
 
 }
--- src/main/java/com/nbc/ecommerceApp/model/Product.java
+++ src/main/java/com/nbc/ecommerceApp/model/Product.java
@@ -3,6 +3,8 @@ package com.nbc.ecommerceApp.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -17,6 +19,8 @@ public class Product {
 
     @Id
     private int id;
+    @NotBlank(message = "Name of the product cannot be blank")
     private String name;
+    @PositiveOrZero(message = "Price cannot be negative")
     private BigDecimal price;
 }
