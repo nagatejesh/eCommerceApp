f7e91b3c5e9d30b8ae0ae9bdf85eecf2e8e64152
Tejesh <nagatejesh@users.noreply.github.com>
2024-07-16 18:40:13 +0000

    In-memeory user details authentication 1

--- src/main/java/com/nbc/ecommerceApp/config/security/SecurityConfig.java
+++ src/main/java/com/nbc/ecommerceApp/config/security/SecurityConfig.java
@@ -2,25 +2,64 @@ package com.nbc.ecommerceApp.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    static SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()); //all endpoints are secured - need authorization
         http.sessionManagement(session->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //session creation policy set to stateless
 //        http.formLogin(withDefaults()); //form login disabled
         http.httpBasic(withDefaults()); //basic authentication is enabled
         return http.build();
+    }
+
+    @Bean
+    static UserDetailsService getUserDetails() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}userpass")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}adminpass")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    static RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.withRolePrefix("")
+                .role("ADMIN").implies("STAFF")
+                .role("STAFF").implies("USER")
+                .role("USER").implies("GUEST")
+                .build();
+    }
+
+    @Bean
+    static MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy);
+        return expressionHandler;
     }
 
 }
