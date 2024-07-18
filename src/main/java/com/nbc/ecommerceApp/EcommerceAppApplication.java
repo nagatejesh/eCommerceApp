c8d17204d79a380a0b423b5923bf89dcdd8bc882
Tejesh <nagatejesh@users.noreply.github.com>
2024-07-18 16:46:35 +0000

    Storing hard coded users in DB
    removed defer data source initialization so the users table will be created before data source setup

--- src/main/java/com/nbc/ecommerceApp/EcommerceAppApplication.java
+++ src/main/java/com/nbc/ecommerceApp/EcommerceAppApplication.java
@@ -1,13 +1,28 @@
 package com.nbc.ecommerceApp;
 
+import com.nbc.ecommerceApp.model.Product;
+import com.nbc.ecommerceApp.service.ProductService;
+import lombok.With;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class EcommerceAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceAppApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(EcommerceAppApplication.class, args);
+
+		ProductService productService = context.getBean(ProductService.class);
+
+		productService.addProduct(Product.builder().name("Sony Xperia 1 II").price(BigDecimal.valueOf(29990.0)).build());
+		productService.addProduct(Product.builder().name("Sony Bravia").price(BigDecimal.valueOf(28000.0)).build());
+		productService.addProduct(Product.builder().name("Sony Alpha 800D").price(BigDecimal.valueOf(56900.0)).build());
+		productService.addProduct(Product.builder().name("Sony Buds Pro 2").price(BigDecimal.valueOf(33499.0)).build());
+		productService.addProduct(Product.builder().name("Sony Headset Z").price(BigDecimal.valueOf(3200.00)).build());
 	}
 
 }
--- src/main/java/com/nbc/ecommerceApp/config/security/SecurityConfig.java
+++ src/main/java/com/nbc/ecommerceApp/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.nbc.ecommerceApp.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
@@ -14,7 +15,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,6 +26,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     static SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -40,7 +47,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    static UserDetailsService getUserDetails() {
+    UserDetailsService getUserDetails() {
         UserDetails user = User.withUsername("user")
                 .password("{noop}userpass")
                 .roles("USER")
@@ -49,7 +56,11 @@ public class SecurityConfig {
                 .password("{noop}adminpass")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+//        return new InMemoryUserDetailsManager(user, admin);
+        return userDetailsManager;
     }
 
     @Bean
